9b7fb696a50a209b350e8a3db98f702bde535018
condegia2005-lgtm <condegia2005@gmail.com>
2025-10-14 20:23:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 20; i++) {
   const img = document.createElement('img');
 
   // Đường dẫn tương đối ổn định trên GitHub Pages
-  img.src = './assets/goat.png';  
+  img.src = './assets/DALL_E_2025-02-11_20.01.18_-_A_chibi-style_goat_wearing_black_sunglasses__pointing_in_one_direction_with_one_hoof_while_holding_a_Sheriff_revolver_from_Valorant_in_the_other_hoof-removebg-preview.png';  
 
   img.alt = "goat bubble";
   bubble.appendChild(img);
@@ -22,3 +22,4 @@ for (let i = 0; i < 20; i++) {
 
   bubblesContainer.appendChild(bubble);
 }
+
